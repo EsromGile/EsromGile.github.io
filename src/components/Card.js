bad247438d4f81281edb9db7da34d073350dabc8
EsromGile <elimorse15@gmail.com>
2022-08-05 00:20:40 -0500

    build

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = (props) => {
     return (
         <div className="card">
             <h2>{props.project}</h2>
-            <img src={props.image}  width="200" height="200"/>
+            <img src={props.image} alt="" width="200" height="200"/>
             <p><a href={props.link}>Link</a></p>
             <ul className="card-languages">
                 {props.languages.map(language => {
